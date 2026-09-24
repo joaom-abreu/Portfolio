4a13b50b4d66e6b691e94cd2b01c4ba23cde7927
joaom-abreu <abreujoaomr@gmail.com>
2026-09-24 03:05:01 -0300

    Estrutura da pagina inicial

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,7 +1,64 @@
 export default function Home() {
-  return (
-      <main>
-        <h1>Portfólio</h1>
-      </main>
-  );
+    return (
+        <>
+            <header>
+                <nav aria-label="Navegação principal">
+                    <a href="#inicio">Início</a>
+                    <a href="#sobre">Sobre</a>
+                    <a href="#projetos">Projetos</a>
+                    <a href="#experiencias">Experiências</a>
+                    <a href="#contato">Contato</a>
+                </nav>
+            </header>
+
+            <main>
+                <section id="inicio">
+                    <h1>João Mário</h1>
+
+                    <p>Estudante de Engenharia de Software</p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque fuga maiores minus omnis tempora?
+                    </p>
+                </section>
+
+                <section id="sobre">
+                    <h2>Sobre Mim</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At deleniti qui recusandae sunt unde?
+                    </p>
+                </section>
+
+                <section id="projetos">
+                    <h2>Projetos</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto cum, ipsum laboriosam molestias quas soluta.
+                    </p>
+                </section>
+
+                <section id="experiencias">
+                    <h2>Experiências</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                        liquid aperiam blanditiis, consequatur esse iusto perspiciatis?
+                    </p>
+                </section>
+
+                <section id="contato">
+                    <h2>Contato</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur.
+                    </p>
+                </section>
+            </main>
+
+            <footer>
+                <p>Portfólio</p>
+            </footer>
+        </>
+    );
 }
